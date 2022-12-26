4d6302355aa3fa1447452271d3ab462aec41c36b
MaratKarsanov <marat.karsanov@mail.ru>
2022-12-26 22:54:37 +0300

    Добавьте файлы проекта.

---
+++ FiveInLineKarsanovM/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace FiveInLineKarsanovM
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Field = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Field
+            // 
+            this.Field.AllowUserToAddRows = false;
+            this.Field.AllowUserToDeleteRows = false;
+            this.Field.AllowUserToResizeColumns = false;
+            this.Field.AllowUserToResizeRows = false;
+            this.Field.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Field.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.Field.GridColor = System.Drawing.Color.Black;
+            this.Field.Location = new System.Drawing.Point(3, 2);
+            this.Field.Name = "Field";
+            this.Field.ReadOnly = true;
+            this.Field.RowTemplate.Height = 50;
+            this.Field.Size = new System.Drawing.Size(454, 454);
+            this.Field.TabIndex = 0;
+            this.Field.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Field_CellMouseClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 50;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 50;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 50;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 50;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 50;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Field);
+            this.Name = "MainForm";
+            this.Text = "Игра \"Пять в ряд\"";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Field;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ FiveInLineKarsanovM/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveInLineKarsanovM
+{
+    public partial class MainForm : Form
+    {
+        const int SIZE = 9;
+        int[,] field = new int[SIZE, SIZE];
+        const int STARTBALLSCOUNT = 7;
+        const int ADDEDBALLSAFTERMOVECOUNT = 3;
+        int transferedBall = 0;
+        int oldCellX = -1;
+        int oldCellY = -1;
+        enum BallType : int
+        {
+            None = 0,
+            Red = 1,
+            Blue = 2,
+            Green = 3,
+            Yellow = 4,
+            Brown = 5,
+            Purple = 6,
+            Pink = 7
+        }
+        //BallType[,] field = new BallType[SIZE, SIZE];
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Field.ColumnHeadersVisible = false;
+            Field.RowHeadersVisible = false;
+            Field.Rows.Add(SIZE);
+            AddNewBalls(STARTBALLSCOUNT);
+            ShowField();
+            //Game();
+        }
+
+        bool isGameOver()
+        {
+            var freeCellsCount = 0;
+            for (var i = 0; i < field.GetLength(0); i++)
+            {
+                for (var j = 0; j < field.GetLength(1); j++)
+                {
+                    if (field[i, j] == 0)
+                        freeCellsCount++;
+                }
+            }
+            if (freeCellsCount < 3)
+                return true;
+            return false;
+        }
+
+        void ShowField()
+        {
+            for (var i = 0; i < SIZE; i++)
+            {
+                for (var j = 0; j < SIZE; j++)
+                {
+                    if (field[i, j] == 1) Field[j, i].Style.BackColor = Color.Red;
+                    else
+                        if (field[i, j] == 2) Field[j, i].Style.BackColor = Color.Blue;
+                    else
+                        if (field[i, j] == 3) Field[j, i].Style.BackColor = Color.Green;
+                    else
+                        if (field[i, j] == 4) Field[j, i].Style.BackColor = Color.Yellow;
+                    else
+                        if (field[i, j] == 5) Field[j, i].Style.BackColor = Color.Brown;
+                    else
+                        if (field[i, j] == 6) Field[j, i].Style.BackColor = Color.Purple;
+                    else
+                        if (field[i, j] == 7) Field[j, i].Style.BackColor = Color.Pink;
+                    else
+                        if (field[i, j] == 0) Field[j, i].Style.BackColor = Color.White;
+                }
+            }
+        }
+
+        void AddNewBalls(int ballsCount)
+        {
+            var rnd = new Random();
+            var x = rnd.Next(0, SIZE - 1);
+            var y = rnd.Next(0, SIZE - 1);
+            var involvedColores = new int[7];
+            var ballColor = rnd.Next(0, involvedColores.Length - 1);
+            for (var i = 0; i < ballsCount; i++)
+            {
+                while (field[x, y] != 0)
+                {
+                    x = rnd.Next(0, SIZE);
+                    y = rnd.Next(0, SIZE);
+                }
+                while (involvedColores[ballColor] != 0)
+                {
+                    ballColor = rnd.Next(0, involvedColores.Length);
+                }
+                field[x, y] = ballColor + 1;
+                involvedColores[ballColor] = 1;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool isCorrectMove()
+        {
+            var wayMatrix = new int[SIZE, SIZE];
+            for (var i = 0; i < SIZE; i++)
+                for (var j = 0; j < SIZE; j++)
+                    if (field[i, j] != 0)
+                        wayMatrix[i, j] = -1;
+            for (var i = 0; i < SIZE; i++)
+            {
+                for (var j = 0; j < SIZE; j++)
+                {
+
+                }
+            }
+            return true;
+        }
+
+        private void Field_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (field[e.RowIndex, e.ColumnIndex] != 0 && transferedBall == 0)
+            {
+                transferedBall = field[e.RowIndex, e.ColumnIndex];
+                oldCellX = e.ColumnIndex;
+                oldCellY = e.RowIndex;
+            }
+            else if (field[e.RowIndex, e.ColumnIndex] == 0 && transferedBall != 0)
+            {
+                field[e.RowIndex, e.ColumnIndex] = transferedBall;
+                field[oldCellY, oldCellX] = 0;
+                oldCellY = oldCellX = -1;
+                transferedBall = 0;
+                AddNewBalls(3);
+                ShowField();
+                if (isGameOver())
+                    MessageBox.Show("Твой счет:");
+            }
+            else if (field[e.RowIndex, e.ColumnIndex] != 0 && transferedBall != 0)
+            {
+                MessageBox.Show("Поле уже занято!");
+                oldCellY = oldCellX = -1;
+                transferedBall = 0;
+            }
+        }
+    }
+}
